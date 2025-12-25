1445c434d7b8ef5655ec424ebb87b3bd8e0b8c93
Matteo Di Sabatino <matteodisabatino@apio.cc>
2025-12-25 02:45:04 +0100

    refactor: using private members instead of WeakMap

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,12 @@ import { to as convertTo } from './libs/convert'
 
 import manifest from '../package.json'
 
-const privates = new WeakMap()
-class TypedEnvPrivates {
-  readonly env: Env
-  constructor (env: Env) {
-    this.env = env
-  }
-}
-
 class TypedEnv {
+  #env: Env
+
   constructor (source_?: object) {
     const source = checkIs(source_, 'object', { allowUndefined: true })
-    const env = new Env(source)
-    privates.set(this, new TypedEnvPrivates(env))
+    this.#env = new Env(source)
   }
 
   static get version (): string {
@@ -28,8 +21,7 @@ class TypedEnv {
     const key = checkIs(key_, 'string') as string
     const toType = checkIs(toType_, 'string') as string
     const defaultValue = checkIs(defaultValue_, toType, { allowUndefined: true })
-    const { env } = privates.get(this) as { env: Env }
-    const val = env.get(key)
+    const val = this.#env.get(key)
     if (typeof val === 'undefined') {
       return defaultValue
     }
@@ -39,16 +31,14 @@ class TypedEnv {
 
   has (key_: string): boolean {
     const key = checkIs(key_, 'string') as string
-    const { env } = privates.get(this) as { env: Env }
-    return env.has(key)
+    return this.#env.has(key)
   }
 
   first (keys_: string[], toType_: string = 'string', defaultValue_?: unknown): any {
     const keys = checkIs(keys_, 'array', { of: 'string' }) as string[]
     const toType = checkIs(toType_, 'string') as string
     const defaultValue = checkIs(defaultValue_, toType, { allowUndefined: true })
-    const { env } = privates.get(this) as { env: Env }
-    const val = env.first(keys)
+    const val = this.#env.first(keys)
     if (typeof val === 'undefined') {
       return defaultValue
     }
@@ -59,27 +49,23 @@ class TypedEnv {
   require (key_: string, toType_: string = 'string'): any {
     const key = checkIs(key_, 'string') as string
     const toType = checkIs(toType_, 'string') as string
-    const { env } = privates.get(this) as { env: Env }
-    const val = env.require(key)
+    const val = this.#env.require(key)
     return convertTo(val, toType)
   }
 
   requireFirst (keys_: string[], toType_: string = 'string'): any {
     const keys = checkIs(keys_, 'array', { of: 'string' }) as string[]
     const toType = checkIs(toType_, 'string') as string
-    const { env } = privates.get(this) as { env: Env }
-    const val = env.requireFirst(keys)
+    const val = this.#env.requireFirst(keys)
     return convertTo(val, toType)
   }
 
   get exists (): any {
-    const { env } = privates.get(this) as { env: Env }
-    return env.exists
+    return this.#env.exists
   }
 
   get required (): any {
-    const { env } = privates.get(this) as { env: Env }
-    return env.required
+    return this.#env.required
   }
 }
 
